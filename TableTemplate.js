bc708d424ca03baed9de7a9a2af2cc58980f2d8d
lvvsndattasai <143427753+lvvsndattasai@users.noreply.github.com>
2023-09-29 05:42:59 -0400

    Added problem 2 solution combined

---
+++ TableTemplate.js
@@ -0,0 +1,57 @@
+// TableTemplate.js
+
+"use strict";
+
+class TableTemplate {
+  static fillIn(id, dict, columnName) {
+    const table = document.getElementById(id);
+
+    if (!table) {
+      console.error(`Table with id '${id}' not found.`);
+      return;
+    }
+
+    const headers = table.rows[0].cells;
+
+    // Replace template strings in header row
+    for (let i = 0; i < headers.length; i++) {
+      const headerText = headers[i].textContent;
+      const replacedText = this.replaceTemplateStrings(headerText, dict);
+      headers[i].textContent = replacedText;
+    }
+
+    if (columnName) {
+      const columnIndex = this.getColumnIndex(headers, columnName);
+
+      if (columnIndex !== -1) {
+        const rows = table.rows;
+
+        // Replace template strings in specified column
+        for (let i = 1; i < rows.length; i++) {
+          const cellText = rows[i].cells[columnIndex].textContent;
+          const replacedText = this.replaceTemplateStrings(cellText, dict);
+          rows[i].cells[columnIndex].textContent = replacedText;
+        }
+      } else {
+        console.error(`Column '${columnName}' not found.`);
+      }
+    }
+
+    table.style.visibility = "visible";
+  }
+
+  static replaceTemplateStrings(text, dict) {
+    return text.replace(/\{\{(\w+)\}\}/g, (match, property) => {
+      return dict[property] || "";
+    });
+  }
+
+  static getColumnIndex(headers, columnName) {
+    for (let i = 0; i < headers.length; i++) {
+      if (headers[i].textContent === columnName) {
+        return i;
+      }
+    }
+    return -1;
+  }
+}
